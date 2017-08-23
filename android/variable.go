687ad5772b5b225a1afa7a73d15679c9b6e4e54f
Simon Shields <simon@lineageos.org>
2017-08-23 12:05:33 +1000

    soong: extend with aim board flags
    Includes the following commit:
    
    Author: Simon Shields <simon@lineageos.org>
    Date:   Mon Sep 4 21:09:44 2017 +1000
    
        soong: explicitly specify name of Aim variables struct
    
        Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15
    
    Change-Id: If127c9da6b57340ae57e0cf725151cdcc3c82ef3

--- android/variable.go
+++ android/variable.go
@@ -20,6 +20,8 @@ import (
 	"runtime"
 	"strings"
 
+	"aim/soong/android"
+
 	"github.com/google/blueprint/proptools"
 )
 
@@ -159,6 +161,9 @@ type variableProperties struct {
 		Device_support_hwfde_perf struct {
 			Cflags []string
 		}
+
+		// include Aim variables
+		Aim android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -372,6 +377,9 @@ type productVariables struct {
 	InstallExtraFlattenedApexes *bool `json:",omitempty"`
 
 	BoardUsesRecoveryAsBoot *bool `json:",omitempty"`
+
+	// include Aim variables
+	Aim android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
